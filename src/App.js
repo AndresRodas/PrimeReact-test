609577b3e80723c65e2a12b5885dbf884bb67b57
Andres Rodas <control.chi10@gmail.com>
2021-09-06 22:47:59 -0600

    Tab menu test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//PRIME
+import 'primereact/resources/themes/saga-blue/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+
+import { Button } from 'primereact/button'
+import { TabMenu } from 'primereact/tabmenu';
+import { render } from '@testing-library/react';
+
+
+export class Menu extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            activeIndex: 0
+        }
+        this.items =  [
+            {label: 'Home', icon: 'pi pi-fw pi-home'},
+            {label: 'Calendar', icon: 'pi pi-fw pi-calendar'},
+            {label: 'Edit', icon: 'pi pi-fw pi-pencil'},
+            {label: 'Documentation', icon: 'pi pi-fw pi-file'},
+            {label: 'Settings', icon: 'pi pi-fw pi-cog'}
+        ];
+    }
+
+    render() {
+      if(this.state.activeIndex == 1){
+        return (
+          <div>
+            <TabMenu model={this.items} activeIndex={this.state.activeIndex} onTabChange={(e) => this.setState({ activeIndex: e.index })} />
+            <Calendar value="Estamos en el Calendario"/>
+          </div>
+        )
+      }
+      if(this.state.activeIndex == 2){
+        return (
+          <div>
+            <TabMenu model={this.items} activeIndex={this.state.activeIndex} onTabChange={(e) => this.setState({ activeIndex: e.index })} />
+            <Edit value="Estamos en el Edit"/>
+          </div>
+        )
+      }
+      return (
+        <div>
+          <TabMenu model={this.items} activeIndex={this.state.activeIndex} onTabChange={(e) => this.setState({ activeIndex: e.index })} />
+          <Home value="Estamos en el Home"/>
+        </div>
+      )
+    }
 }
+
+class Home extends React.Component {
+  render() {
+    return (
+      <form>
+        <h1>HOME</h1>
+        <h2>valor: {this.props.value}</h2>
+      </form>
+    )
+  }
+}
+class Calendar extends React.Component {
+  render() {
+    return (
+      <form>
+        <h1>CALENDAR</h1>
+        <h2>valor: {this.props.value}</h2>
+      </form>
+    )
+  }
+}
+class Edit extends React.Component {
+  render() {
+    return (
+      <form>
+        <h1>EDIT</h1>
+        <h2>valor: {this.props.value}</h2>
+      </form>
+    )
+  }
+}
+
+
+
+
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Menu />
+      </div>
+    )
+  }
+}
+
+
 
 export default App;
